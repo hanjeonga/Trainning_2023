5a1a736ec053f1f35671bdf15deb4b04c1238c54
Jung-A Han <97014347+hanjeonga@users.noreply.github.com>
2023-02-15 15:59:38 +0900

    html/scss

--- DatePicker/src/js/index.js
+++ DatePicker/src/js/index.js
@@ -1,1 +1,96 @@
-console.log("hi");
+class DatePicker {
+  monthData = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  #calendarDate = {
+    data: "",
+    date: 0,
+    month: 0,
+    year: 0,
+  };
+
+  selectedDate = {
+    data: "",
+    date: 0,
+    month: 0,
+    year: 0,
+  };
+
+  datePickerEl;
+  dateInputEl;
+  calendarEl;
+  calendarMonthEl;
+  monthContentEl;
+  nextBtnEl;
+  prevBtnEl;
+  calendarDatesEl;
+
+  constructor() {
+    this.initCalendarDate();
+    this.assignElement();
+    this.addEvent();
+  }
+
+  initCalendarDate() {
+    const data = new Date();
+    const date = data.getDate();
+    const month = data.getMonth();
+    const year = data.getFullYear();
+    this.#calendarDate = {
+      data,
+      date,
+      month,
+      year,
+    };
+  }
+
+  assignElement() {
+    this.datePickerEl = document.getElementById("date-picker");
+    this.dateInputEl = this.datePickerEl.querySelector("#date-input");
+    this.calendarEl = this.datePickerEl.querySelector("#calendar");
+    this.calendarMonthEl = this.calendarEl.querySelector("#month");
+    this.monthContentEl = this.calendarMonthEl.querySelector("#content");
+    this.nextBtnEl = this.calendarMonthEl.querySelector("#next");
+    this.prevBtnEl = this.calendarMonthEl.querySelector("#prev");
+    this.calendarDatesEl = this.calendarEl.querySelector("#dates");
+  }
+
+  addEvent() {
+    this.dateInputEl.addEventListener("click", this.togglecalendar.bind(this));
+  }
+
+  togglecalendar() {
+    this.calendarEl.classList.toggle("active");
+    this.updateMonth();
+    this.updateDates();
+  }
+
+  updateMonth() {
+    this.monthContentEl.textContent = `${this.#calendarDate.year} ${
+      this.monthData[this.#calendarDate.month]
+    }`;
+  }
+
+  updateDates() {
+    this.calendarDatesEl.innerHTML = "";
+    const numberOfDates = new Date(
+      this.#calendarDate.year,
+      this.#calendarDate.month + 1,
+      0
+    ).getDate();
+  }
+}
+
+new DatePicker();
